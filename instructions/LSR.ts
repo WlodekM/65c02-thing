7d5b4d78bf24ebcae4bd8957e91aba2541a26686
WlodekM <wlodzio.maly@gmail.com>
2025-04-02 19:42:29 +0300

    some more

---
+++ instructions/LSR.ts
@@ -0,0 +1,23 @@
+import type The65c02 from "../65c02.ts";
+
+export default function (this: The65c02, mode: string) {
+    if (mode == 'implicit') {
+        const mem = this.regA.num();
+        const result = mem >> 1;
+        this.negative = (result & 128) != 0
+        this.zero = result == 0
+        this.carry = (mem & 1) != 0
+        this.regA.set(result & 0xFF);
+    } else {
+        const addr = this.getAddr(mode)
+        this.io.address.set(addr);
+        this.read()
+        const mem = this.io.data.num();
+        const result = mem >> 1;
+        this.negative = (result & 128) != 0
+        this.zero = result == 0
+        this.carry = (mem & 1) != 0
+        this.io.data.set(result & 0xFF);
+        this.write()
+    }
+}
