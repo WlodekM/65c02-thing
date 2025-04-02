eb5a89d914d86a3b094d6d143e01cac48ae8b128
WlodekM <wlodzio.maly@gmail.com>
2025-04-02 18:11:35 +0300

    the other branches

---
+++ instructions/BVS.ts
@@ -0,0 +1,7 @@
+import type The65c02 from "../65c02.ts";
+
+export default function (this: The65c02, mode: string) {
+    const location = this.getAddr(mode, ['relative', 'absolute']);
+    if (this.overflow)
+        this.programCounter.set(location)
+}
